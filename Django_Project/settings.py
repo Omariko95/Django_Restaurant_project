2539c8f072a9e8608460047f0b8e9b62a3eb6029
Omar Moawad <114612179+Omariko95@users.noreply.github.com>
2023-01-24 00:35:58 -0500

    Main App Urls updated

--- Django_Project/settings.py
+++ Django_Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Django_delights'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
+
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ Django_delights/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+	#path("", views.index, name="index"),
+  #path("account/", include("django.contrib.auth.urls")),
+  #path("signup/", views.SignUp.as_view() , name="signup"),
+]
