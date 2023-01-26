bc7bc8c2b3fba0b4c5d0367502d181dea16b5d72
Omar Moawad <114612179+Omariko95@users.noreply.github.com>
2023-01-26 02:09:33 -0500

    urls edit

--- Django_delights/urls.py
+++ Django_delights/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	#path("", views.index, name="index"),
+	path("", views.HomeView.as_view() , name="home"),
   #path("account/", include("django.contrib.auth.urls")),
 ]
