4011e1280ed6fb69beb5ecef5521e5245cb369b2
Omar Moawad <114612179+Omariko95@users.noreply.github.com>
2023-01-26 01:56:24 -0500

    Edits

--- Django_Project/urls.py
+++ Django_Project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path("", include("Django_delights.urls")),
-    path('admin/', admin.site.urls),
+    path('', include('Django_delights.urls')),
+    path('admin/', admin.site.urls)
 ]
